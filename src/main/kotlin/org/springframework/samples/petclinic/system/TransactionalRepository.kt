2a5a446be819ab26c582883c3267480ee2935d28
Test Name <test@unittest.com>
2024-04-30 17:10:58 +0200

    fix changes

--- src/main/kotlin/org/springframework/samples/petclinic/system/TransactionalRepository.kt
+++ src/main/kotlin/org/springframework/samples/petclinic/system/TransactionalRepository.kt
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Suppress("JvmCoverageInspection")
 class TransactionalRepository(val pets: PetRepository, val owners: OwnerRepository) {
     @Transactional
     fun saveOwnerAndPet(pet: Pet, owner: Owner) {
